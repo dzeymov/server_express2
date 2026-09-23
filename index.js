fe64c131b17354a7d9eabf6938e452489044b25e
yahaaaash <dzeytovskiy@gmail.com>
2026-09-23 10:51:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const http = require('http')
 const express = require("express")
 const app = express()
 
 const PORT = 3000;
 
+
 app.get("/", (req, res)=>{
     res.send("Главная страница")
 })
+
+app.get("/about", (req, res)=>{
+    res.send("О нас")
+})
+
+app.get("/contacts", (req, res)=>{
+    res.send("Наши контакты 8999-111-22-33")
+})
+
+app.get('/product/:id', (req ,res)=>{
+console.log(req.params);
+res.send(`Товар с id: ${req.params.id}`)
+
+})
+
+
+app.get('/phone/:number', (req ,res)=>{
+    console.log(req.params);
+    res.send(`Ваш номер телефона: ${req.params.number}`)
+    
+    })
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Сервер запущен на localhost:${PORT}`);
